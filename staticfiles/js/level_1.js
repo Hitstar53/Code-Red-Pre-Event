8f4a04907137909363cf9b2116b7b6629920e984
Hatim Sawai <hatimsawai76@gmail.com>
2023-03-07 19:58:57 +0530

    deploy #2
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- staticfiles/js/level_1.js
+++ staticfiles/js/level_1.js
@@ -1,24 +1,24 @@
+
 const imageBox = document.querySelector(".image-box");
 const img = imageBox.querySelector("img");
 const closeBtn = imageBox.querySelector(".close-btn");
 
-img.addEventListener("click", function() {
-  imageBox.style.width = "80%";
-  imageBox.style.height = "80%";
+img.addEventListener("click", function () {
+  imageBox.style.width = "70%";
+  imageBox.style.height = "70%";
   imageBox.style.position = "absolute";
-    // imageBox.style.top = "50%";
-    // imageBox.style.left = "50%";
-    imageBox.style.marginTop = "0";
-    imageBox.style.marginLeft = "-1rem";
-    // imageBox.style.transform = "translate(-50%, -50%)";
+  imageBox.style.marginTop = "0";
+  imageBox.style.marginLeft = "-1rem";
+  imageBox.style.transform = "translate(-50%, -50%)";
   closeBtn.style.display = "block";
 });
 
 closeBtn.addEventListener("click", function() {
   imageBox.style.width = "140px";
   imageBox.style.height = "140px";
-  imageBox.style.marginTop = "-6rem";
-    imageBox.style.marginLeft = "42rem";
+  imageBox.style.marginTop = "0";
+  imageBox.style.marginLeft = "42rem";
+  imageBox.style.transform = "translate(-50%, -145%)";
   closeBtn.style.display = "none";
 });
 
@@ -51,6 +51,71 @@ submit.addEventListener("click", function(event) {
 
   }
 })
+
+
+function stopwatch(elementName) {
+  var appendTens = document.getElementById("tens");
+  var appendSeconds = document.getElementById("seconds");
+  var appendMinutes = document.getElementById("minutes");
+  const btn = document.getElementById("hint");
+
+  
+  var tens = parseInt(localStorage.getItem("tens")) || 0;
+  var seconds = parseInt(localStorage.getItem("seconds")) || 0;
+  var minutes = parseInt(localStorage.getItem("minutes")) || 0;
+
+ 
+  appendTens.innerHTML = formatTime(tens);
+  appendSeconds.innerHTML = formatTime(seconds);
+  appendMinutes.innerHTML = formatTime(minutes);
+
+
+  var interval = setInterval(startTimer, 10);
+
+  function startTimer() {
+    tens++;
+    if (tens > 99) {
+      seconds++;
+      tens = 0;
+    }
+    if (seconds > 59) {
+      minutes++;
+      seconds = 0;
+    }
+    
+    localStorage.setItem("tens", tens);
+    localStorage.setItem("seconds", seconds);
+    localStorage.setItem("minutes", minutes);
+
+ 
+    appendTens.innerHTML = formatTime(tens);
+    appendSeconds.innerHTML = formatTime(seconds);
+    appendMinutes.innerHTML = formatTime(minutes);
+
+    
+    if (minutes > 0) {
+      btn.disabled = false;
+      btn.style.border = "none";
+      btn.style.color = "black";
+      btn.style.backgroundColor = "yellow";
+      btn.style.cursor = "pointer";
+    }
+  }
+
+
+  function formatTime(time) {
+    return time.toString().padStart(2, "0");
+  }
+}
+
+window.onbeforeunload = function() {
+  localStorage.clear();
+}
+
+
+
+stopwatch("stopwatch");
+localStorage.clear();
 
 
 
--- staticfiles/js/level_2.js
+++ staticfiles/js/level_2.js
@@ -38,3 +38,67 @@ function check(){
 
 }
 
+function stopwatch(elementName) {
+    var appendTens = document.getElementById("tens");
+    var appendSeconds = document.getElementById("seconds");
+    var appendMinutes = document.getElementById("minutes");
+    const btn = document.getElementById("hint");
+  
+    // Load the timer values from local storage
+    var tens = parseInt(localStorage.getItem("tens_2")) || 0;
+    var seconds = parseInt(localStorage.getItem("seconds_2")) || 0;
+    var minutes = parseInt(localStorage.getItem("minutes_2")) || 0;
+  
+    // Display the timer values
+    appendTens.innerHTML = formatTime(tens);
+    appendSeconds.innerHTML = formatTime(seconds);
+    appendMinutes.innerHTML = formatTime(minutes);
+  
+    // Start the timer
+    var interval = setInterval(startTimer, 10);
+  
+    function startTimer() {
+      tens++;
+      if (tens > 99) {
+        seconds++;
+        tens = 0;
+      }
+      if (seconds > 59) {
+        minutes++;
+        seconds = 0;
+      }
+      // Save the timer values to local storage
+      localStorage.setItem("tens_2", tens);
+      localStorage.setItem("seconds_2", seconds);
+      localStorage.setItem("minutes_2", minutes);
+  
+      // Display the timer values
+      appendTens.innerHTML = formatTime(tens);
+      appendSeconds.innerHTML = formatTime(seconds);
+      appendMinutes.innerHTML = formatTime(minutes);
+  
+      // Enable the button when the timer reaches 1 minute
+      if (minutes > 0) {
+        btn.disabled = false;
+        btn.style.border = "none";
+        btn.style.color = "black";
+        btn.style.backgroundColor = "yellow";
+        btn.style.cursor = "pointer";
+      }
+    }
+  
+    // Format the time value with leading zeros
+    function formatTime(time) {
+      return time.toString().padStart(2, "0");
+    }
+  }
+  
+  window.onbeforeunload = function() {
+    localStorage.clear();
+  }
+  
+  
+  
+  stopwatch("stopwatch");
+  localStorage.clear();
+
